2487cb47fcadd8b4cd9b7867d7cfd9fdf6e8f855
HallRobotics1697 <hallroboticswhps@gmail.com>
2024-09-23 08:49:57 -0400

    build bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "vex.h"
 
 using namespace vex;
+using std::abs;
 
 // A global instance of competition
 competition Competition = competition();
@@ -18,8 +19,8 @@ controller Controller1 = controller();
 controller Controller2 = controller(); // Maybe dual controller
 //drive motors
 motor left_front = motor(PORT1, ratio6_1, 0); // 11w
-motor left_back = motor(PORT2, ratio6_1, 0); // 11w
-motor right_front = motor(PORT3, ratio6_1, 0); // 11w
+motor left_back = motor(PORT2, ratio6_1, 1); // 11w
+motor right_front = motor(PORT3, ratio6_1, 1); // 11w
 motor right_back = motor(PORT4, ratio6_1, 0); // 11w
 motor_group leftMotors = motor_group(left_front, left_back);
 motor_group rightMotors = motor_group(right_front, right_back);
@@ -38,36 +39,60 @@ inertial Inertial = inertial(PORT10);
 rotation Rot = rotation(PORT11);
 vision Vision = vision(PORT11); // Unlikely
 
-void usercontrol(void) {
+class PIDCtrl {
+  private:
+    float kP, kI, kD, error, integral, derriv, prevError, (* sense_val) (void);
+    int delay, integral_threshold;
+  public:
+    void init_PID(float kP, float kI, float kD, float(*callback)(void), int delay = 15) {
+      this->kP = kP;
+      this->kI = kI;
+      this->kD = kD;
+      this->delay = delay;
+      this->integral_threshold = 1000000;
+      this->integral = 0;
+      this->sense_val = callback;
+    }
+    float update(float target){
+      this->error = target - sense_val();
+      if(fabsf(error) < integral_threshold)
+        integral += error;
+      else 
+        integral = 0;
+      derriv = error - prevError;
+      prevError = error;
+      return kP*error + kI*integral + kD*derriv;
+    }
 
-  Controller1.ButtonR2.pressed([](void){roller.spin(fwd,100,pct);});
-  Controller1.ButtonR2.released([](void){roller.stop(coast);});
-  Controller1.ButtonL2.pressed([](void){grabber.spinFor(60,degrees); grabber.stop(hold);}); // TBD
-  Controller1.ButtonL2.released([](void){grabber.stop(coast);});
-  Controller1.Axis2.changed(lift); //or create a thread with lift in while loop
+};
 
+void trap_acc_drive() {
+  float k = 100;
   while(1) {
-    int x = Controller1.Axis4.position();
     int y = Controller1.Axis3.position();
+    int x = Controller1.Axis1.position();
 
     if(abs(x) < 10)
       x = 0;
     if(abs(y) < 10)
       y = 0;
-
-    if(y+x != 0)
-      leftMotors.spin(fwd,(y+x),pct);
+    float leftVel = leftMotors.velocity(pct);
+    float rightVel = rightMotors.velocity(pct);
+    leftVel += k*((x+y > leftVel) - (x+y < leftVel));
+    rightVel += k*((x-y > rightVel) - (x-y < rightVel));
+    if(abs(leftVel) > 0)
+      leftMotors.spin(fwd,leftVel,pct);
     else
       leftMotors.stop(coast);
     
-    if(y-x != 0)
-      rightMotors.spin(fwd,(y-x),pct);
+    if(abs(rightVel) > 0)
+      rightMotors.spin(fwd,rightVel,pct);
     else
       rightMotors.stop(coast);
-    wait(5,msec); // So the cortex doesn't overload
+
+    wait(25,msec); // So the cortex doesn't overload
   } 
 }
-
 
 void pid_drive() {
   PIDCtrl PIDLateral;
@@ -79,8 +104,8 @@ void pid_drive() {
   float turn = 0;
  
   while(1) {
-    int x = Controller1.Axis4.position()*600;
     int y = Controller1.Axis3.position()*600;
+    int x = Controller1.Axis1.position()*600;
   
     lateral = PIDLateral.update(y);
     turn = PIDRot.update(x);
@@ -112,33 +137,61 @@ void eat_mogo() {
   }
 }
 
+void usercontrol(void) {
+  Controller1.ButtonR2.pressed([](void){roller.spin(fwd,100,pct);});
+  Controller1.ButtonR2.released([](void){roller.stop(coast);});
+  Controller1.ButtonL2.pressed([](void){grabber.spinFor(60,degrees); grabber.stop(hold);}); // TBD
+  Controller1.ButtonL2.released([](void){grabber.stop(coast);});
+  Controller1.Axis2.changed(lift); //or create a thread with lift in while loop
+
+  while(1) {
+    int y = Controller1.Axis3.position();
+    int x = Controller1.Axis1.position();
+
+    if(abs(x) < 10)
+      x = 0;
+    if(abs(y) < 10)
+      y = 0;
+
+    if(y+x != 0)
+      leftMotors.spin(fwd,(y+x),pct);
+    else
+      leftMotors.stop(coast);
+    
+    if(y-x != 0)
+      rightMotors.spin(fwd,(y-x),pct);
+    else
+      rightMotors.stop(coast);
+    wait(5,msec); // So the cortex doesn't overload
+  } 
+}
+
 float trap_target(int acc_msec, int time, float t) {
   if(t < 0 || t > time)
     return 0;
-  if(t*time <= acc_msec)
+  if(t <= acc_msec)
     return t/acc_msec;
-  if(t*time >= time - acc_msec)
+  if(t >= time - acc_msec)
     return (time-t)/acc_msec;
   return 1;
 }
+ 
 
-void pid_cm(float dist = 0, float turn = 0, float m_sec, float maxspeed = 1) {
+void pid_cm(float dist = 0, float turn = 0, float maxspeed = 1) {
   timer Timer = timer();
   PIDCtrl drivePID;
+  float acc_msec = 2;
   drivePID.init_PID(0.0f,0.0f,0.0f,[](void) -> float{return Inertial.gyroRate(yaxis,rpm);});
-  drivePID.init_PID(0.0f,0.0f,0.0f,[](void) -> float{return Rot.velocity(rpm);});
-  float time = dist;
+  //drivePID.init_PID(0.0f,0.0f,0.0f,[](void) -> float{return Rot.velocity(rpm);});
+  float time = dist/(600*maxspeed) + acc_msec;
   float power = 0;
   while(1) {
-    power = drivePID.update(trap_target(2,time,Timer.time(sec)));
+    power = drivePID.update(trap_target(acc_msec,time,Timer.time(sec)));
     leftMotors.spin(fwd,power,rpm);
     rightMotors.spin(fwd,power,rpm);
   }
 }
 
-void pid_vel() {
-
-}
 
 void auton_1() {
 
@@ -155,16 +208,10 @@ void pre_auton(void) {
   mogoMotor.resetPosition();
 
   if(Inertial.installed()) {
+    Brain.Screen.printAt(0,10,0,"Inertial is calibrating");
     Inertial.calibrate();
-    while(Inertial.isCalibrating()) {
-      Brain.Screen.printAt(0,10,0,"Inertial is calibrating.  ");
-      wait(200,msec);
-      Brain.Screen.printAt(0,10,0,"Inertial is calibrating.. ");
-      wait(200,msec);
-      Brain.Screen.printAt(0,10,0,"Inertial is calibrating...");
-      wait(200,msec);
-    }
-    Brain.Screen.printAt(0,10,0,"                          ");
+    waitUntil(!Inertial.isCalibrating());
+    Brain.Screen.printAt(0,10,0,"                       ");
   }
   else
     Brain.Screen.printAt(0,10,0,"Inertial is not installed!");
@@ -177,7 +224,7 @@ void pre_auton(void) {
 
 int main() {
 
-  pre_auton();
+  //pre_auton();
   
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
@@ -186,30 +233,3 @@ int main() {
     wait(100, msec);
   }
 }
-
-class PIDCtrl {
-  private:
-    float target = 0, kP, kI, kD, error, integral, derriv, prevError, (* sense_val) (void);
-    int delay, integral_threshold;
-  public:
-    void init_PID(float kP, float kI, float kD, float(*callback)(void), int delay = 15) {
-      this->kP = kP;
-      this->kI = kI;
-      this->kD = kD;
-      this->delay = delay;
-      this->integral_threshold;
-      this->integral = 0;
-      this->sense_val = callback;
-    }
-    float update(float target){
-      this->error = target - sense_val();
-      if(abs(error) < integral_threshold)
-        integral += error;
-      else 
-        integral = 0;
-      derriv = error - prevError;
-      prevError = error;
-      return kP*error + kI*integral + kD*derriv;
-    }
-
-};
