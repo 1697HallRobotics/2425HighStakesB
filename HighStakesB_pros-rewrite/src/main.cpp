c79c00d8b429c5d343bee462784fb8ec40fb2370
HallRobotics1697 <hallroboticswhps@gmail.com>
2025-01-13 21:39:31 -0500

    buttons wip

--- HighStakesB_pros-rewrite/src/main.cpp
+++ HighStakesB_pros-rewrite/src/main.cpp
@@ -1,7 +1,14 @@
 #include "main.h"
+#include "liblvgl/core/lv_event.h"
+#include "liblvgl/core/lv_obj_pos.h"
+#include "liblvgl/draw/lv_draw_rect.h"
+#include "liblvgl/misc/lv_color.h"
+#include "liblvgl/widgets/lv_btnmatrix.h"
 #include "pros/adi.hpp"
 #include "pros/misc.h"
 #include "pros/motors.h"
+#include "pros/apix.h"
+#include <cstddef>
 
 #define LEFT_MOTOR_PORTS {7, 5}
 #define RIGHT_MOTOR_PORTS {8, 19}
@@ -14,7 +21,52 @@
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {}
+
+static void event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * obj = lv_event_get_target(e);
+    if(code == LV_EVENT_VALUE_CHANGED) {
+        uint32_t id = lv_btnmatrix_get_selected_btn(obj);
+        const char * txt = lv_btnmatrix_get_btn_text(obj, id);
+
+        LV_LOG_USER("%s was pressed\n", txt);
+    }
+	if(code == LV_EVENT_DRAW_PART_BEGIN) {
+		lv_obj_draw_part_dsc_t * dsc = (lv_obj_draw_part_dsc_t *)lv_event_get_param(e);
+		 //Colors the top buttons BLUE
+        if(dsc->id == 0 || dsc -> id == 1) {
+            dsc->rect_dsc->radius = LV_RADIUS_CIRCLE;
+            if(lv_btnmatrix_get_selected_btn(obj) == dsc->id)  dsc->rect_dsc->bg_color = lv_palette_darken(LV_PALETTE_BLUE, 3);
+            else dsc->rect_dsc->bg_color = lv_palette_main(LV_PALETTE_BLUE);
+            dsc->label_dsc->color = lv_color_white();
+        }
+		//Colors the bottom buttons RED
+        else if(dsc->id == 2 || dsc->id == 3) {
+            dsc->rect_dsc->radius = LV_RADIUS_CIRCLE;
+            if(lv_btnmatrix_get_selected_btn(obj) == dsc->id)  dsc->rect_dsc->bg_color = lv_palette_darken(LV_PALETTE_RED, 3);
+            else dsc->rect_dsc->bg_color = lv_palette_main(LV_PALETTE_RED);
+            dsc->label_dsc->color = lv_color_white();
+        }
+		//Colors the skill's button PURPLE
+		else if(dsc->id == 5) {
+            dsc->rect_dsc->radius = LV_RADIUS_CIRCLE;
+            if(lv_btnmatrix_get_selected_btn(obj) == dsc->id)  dsc->rect_dsc->bg_color = lv_palette_darken(LV_PALETTE_PURPLE, 3);
+            else dsc->rect_dsc->bg_color = lv_palette_main(LV_PALETTE_PURPLE);
+            dsc->label_dsc->color = lv_color_white();
+        }
+	}
+}
+
+static const char * btnm_map[] = {"L","R","\n","L","R","\n","SKILLS"};
+
+void initialize() {
+    lv_obj_t * btnm1 = lv_btnmatrix_create(lv_scr_act());
+    lv_btnmatrix_set_map(btnm1, btnm_map);
+    //lv_obj_align(btnm1, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_event_cb(btnm1, event_handler, LV_EVENT_ALL, NULL);
+	lv_obj_center(btnm1);
+}
 
 /**
  * Runs while the robot is in the disabled state
@@ -30,6 +82,12 @@ void competition_initialize() {}
  */
 void autonomous() {}
 
+void auton_LR() {}
+void auton_RR() {}
+void auton_LB() {}
+void auton_RB() {}
+void auton_SKILLS() {}
+
 /**
  */
 void opcontrol() {
@@ -43,15 +101,17 @@ void opcontrol() {
 	right_mg.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	intake_mg.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 
-
 	while (1) {
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		
+
+		//if joystick is in deadzone, it counts as zero
+		//prevents unintentional drift
 		if(abs(dir) < DEADZONE)
 			dir = 0;
 		if(abs(turn) < DEADZONE)
 			turn = 0;
+
 		if(dir && turn == 0) {
 			left_mg.brake();
 			right_mg.brake();
